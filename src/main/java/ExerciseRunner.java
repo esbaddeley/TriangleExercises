a6f8009c627b92eae5f5db27530cec52224612fc
esbaddeley <esbaddeley@gmail.com>
2016-09-08 16:04:06 +0100

    Refactored to create an exercise runner

---
+++ src/main/java/ExerciseRunner.java
@@ -0,0 +1,9 @@
+/**
+ * Created by emmabaddeley on 08/09/2016.
+ */
+public class ExerciseRunner {
+    public static void main (String[] args){
+        new EasyExercise().run();
+
+    }
+}
---
+++ src/main/java/VerticalLine.java
@@ -0,0 +1,7 @@
+/**
+ * Created by emmabaddeley on 08/09/2016.
+ */
+public class VerticalLine {
+
+
+}
