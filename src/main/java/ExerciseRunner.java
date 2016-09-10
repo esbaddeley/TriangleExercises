14772024a58d465d0ac73156f172dd81b061efcf
esbaddeley <esbaddeley@gmail.com>
2016-09-10 15:29:05 +0100

    Added Horizontal Line generator

--- src/main/java/ExerciseRunner.java
+++ src/main/java/ExerciseRunner.java
@@ -5,5 +5,6 @@ public class ExerciseRunner {
     public static void main (String[] args){
         new EasyExercise().run();
         new VerticalLine().run(8);
+        new HorizontalLine().run(8);
     }
 }
---
+++ src/main/java/HorizontalLine.java
@@ -0,0 +1,11 @@
+/**
+ * Created by emmabaddeley on 10/09/2016.
+ */
+public class HorizontalLine {
+
+    public void run (int repeater){
+        for (int r = 0; r < repeater; r++){
+            System.out.println("*");
+        }
+    }
+}
